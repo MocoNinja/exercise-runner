5cf1ed18c8acea53de7bbf712d8ec759dbef2eaa
MocoNinja <javiergonzaleztello@gmail.com>
2025-07-07 17:59:58 +0200

    Jugando con plaquitas

--- src/main/java/es/moconinja/exerciserunner/launcher/Launcher.java
+++ src/main/java/es/moconinja/exerciserunner/launcher/Launcher.java
@@ -5,7 +5,8 @@ import es.moconinja.exerciserunner.exercise.BaseExercise;
 /**
  * Simple class to launch any {@link BaseExercise} from a main or whatever
  */
-public class Launcher {
+public final class Launcher {
+    private Launcher() {}
     public static void launch(final BaseExercise exercise) {
         exercise.launchExercise();
     }
